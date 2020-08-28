66cb0c160b4fa79006be6c11d48043f76c628e09
Yama-Tomo <mazda13bturbo@gmail.com>
2020-08-28 18:41:42 +0900

    feat: determine launch port of server from env vars

--- src/graphql-server/server.ts
+++ src/graphql-server/server.ts
@@ -1,10 +1,13 @@
 import { createServer } from 'http';
+import path from 'path';
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import { createContext } from './context';
 import { schema } from './nexus-schema-gen';
 
-const { PORT = 5000 } = process.env;
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+const { GRAPHQL_PORT = 5000 } = process.env;
 
 const app = express();
 const server = createServer(app);
@@ -17,6 +20,8 @@ const apollo = new ApolloServer({
 
 apollo.applyMiddleware({ app });
 
-server.listen({ port: PORT }, () => {
-  process.stdout.write(`🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}\n`);
+server.listen({ port: GRAPHQL_PORT }, () => {
+  process.stdout.write(
+    `🚀 Server ready at http://localhost:${GRAPHQL_PORT}${apollo.graphqlPath}\n`
+  );
 });
